6a5d4ed42ff35d9e7b8629aaa05a373065acc1af
robert li <silent.xx@gmail.com>
2016-05-30 16:54:54 +0800

    GFW.Sharp Core encrypt/decrypt

---
+++ GFW.Sharp/GFW.Sharp.Core/Ciphering/SecretKey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GFW.Sharp.Core.Ciphering
+{
+    public class SecretKey
+    {
+        public byte[] Key
+        { get; set; }
+        public string Algorithym
+        { get; set; }
+    }
+}
---
+++ GFW.Sharp/GFW.Sharp.Test/Program.cs
@@ -0,0 +1,35 @@
+﻿using GFW.Sharp.Core.Ciphering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GFW.Sharp.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encrypt aes = new Encrypt();
+
+            // 文件加密测试
+            // aes.testEncryptFile();
+
+            // 测试
+            //aes.testSecureRandom();
+
+            //aes.testIsPassword();
+
+            //aes.test();
+            byte[] b = new byte[1024];
+            b[512] = 97;
+
+            SecretKey key = aes.getKey();
+            byte[] encrypted = aes.encryptNet(key, b);
+            byte[] decrypted = aes.decrypt(key, encrypted);
+
+            System.Console.ReadLine();
+        }
+    }
+}
