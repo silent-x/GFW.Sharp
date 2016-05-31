972678ded3bd748fce827cf4404663c07e30005b
robert li <silent.xx@gmail.com>
2016-06-01 00:42:35 +0800

    move forward method to instance

--- GFW.Sharp/GFW.Sharp.Core/Forward/Forwarder.cs
+++ GFW.Sharp/GFW.Sharp.Core/Forward/Forwarder.cs
@@ -123,52 +123,8 @@ namespace GFW.Sharp.Core.Forward
         }
         ///<summary>Starts relaying data between the remote host and the local client.</summary>
         ///<remarks>This method should only be called after all protocol specific communication has been finished.</remarks>
-        public void StartForward()
-        {
-            try
-            {
-                ClientSocket.BeginReceive(Buffer, 0, Buffer.Length, SocketFlags.None, new AsyncCallback(this.OnClientReceive), ClientSocket);
-            }
-            catch
-            {
-                Dispose();
-            }
-        }
-        ///<summary>Called when we have received data from the local client.<br>Incoming data will immediately be forwarded to the remote host.</br></summary>
-        ///<param name="ar">The result of the asynchronous operation.</param>
-        protected void OnClientReceive(IAsyncResult ar)
-        {
-            try
-            {
-                int Ret = ClientSocket.EndReceive(ar);
-                if (Ret <= 0)
-                {
-                    Dispose();
-                    return;
-                }
-                DestinationSocket.BeginSend(Buffer, 0, Ret, SocketFlags.None, new AsyncCallback(this.OnRemoteSent), DestinationSocket);
-            }
-            catch
-            {
-                Dispose();
-            }
-        }
-        ///<summary>Called when we have sent data to the remote host.<br>When all the data has been sent, we will start receiving again from the local client.</br></summary>
-        ///<param name="ar">The result of the asynchronous operation.</param>
-        protected void OnRemoteSent(IAsyncResult ar)
-        {
-            try
-            {
-                int Ret = DestinationSocket.EndSend(ar);
-                if (Ret > 0)
-                {
-                    ClientSocket.BeginReceive(Buffer, 0, Buffer.Length, SocketFlags.None, new AsyncCallback(this.OnClientReceive), ClientSocket);
-                    return;
-                }
-            }
-            catch { }
-            Dispose();
-        }
+        public abstract void StartForward();
+        
         
     }
 }
---
+++ GFW.Sharp/GFW.Sharp.Core/Forward/Transparent/TransparenetForwarder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GFW.Sharp.Core.Forward.Transparent
+{
+    public class TransparentForwarder : Forwarder
+    {
+        //private IPEndPoint m_MapTo;
+        public TransparentForwarder(Socket ClientSocket, DestroyDelegate Destroyer, Socket DestinationSocket) : base(ClientSocket, Destroyer)
+        {
+            //this.MapTo = MapTo;
+            this.DestinationSocket = DestinationSocket;
+        }
+
+        public override void StartForward()
+        {
+            try
+            {
+                ClientSocket.BeginReceive(Buffer, 0, Buffer.Length, SocketFlags.None, new AsyncCallback(this.OnClientReceive), ClientSocket);
+            }
+            catch
+            {
+                Dispose();
+            }
+        }
+        ///<summary>Called when we have received data from the local client.<br>Incoming data will immediately be forwarded to the remote host.</br></summary>
+        ///<param name="ar">The result of the asynchronous operation.</param>
+        protected void OnClientReceive(IAsyncResult ar)
+        {
+            try
+            {
+                int Ret = ClientSocket.EndReceive(ar);
+                if (Ret <= 0)
+                {
+                    Dispose();
+                    return;
+                }
+                DestinationSocket.BeginSend(Buffer, 0, Ret, SocketFlags.None, new AsyncCallback(this.OnRemoteSent), DestinationSocket);
+            }
+            catch
+            {
+                Dispose();
+            }
+        }
+        ///<summary>Called when we have sent data to the remote host.<br>When all the data has been sent, we will start receiving again from the local client.</br></summary>
+        ///<param name="ar">The result of the asynchronous operation.</param>
+        protected void OnRemoteSent(IAsyncResult ar)
+        {
+            try
+            {
+                int Ret = DestinationSocket.EndSend(ar);
+                if (Ret > 0)
+                {
+                    ClientSocket.BeginReceive(Buffer, 0, Buffer.Length, SocketFlags.None, new AsyncCallback(this.OnClientReceive), ClientSocket);
+                    return;
+                }
+            }
+            catch { }
+            Dispose();
+        }
+    }
+}
